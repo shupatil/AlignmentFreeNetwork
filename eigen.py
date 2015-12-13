afa182b7c688bb06d05ecdacfa8cda6b7d599398
aghanta <aghanta@cs.stonybrook.edu>
2015-12-13 14:46:34 -0500

    Latest code of eigen values signature impelmentation

--- eigen.py
+++ eigen.py
@@ -5,87 +5,119 @@ import numpy as np
 from time import clock
 from scipy.linalg import eigh as largest_eigh
 from scipy.sparse.linalg.eigen.arpack import eigsh as largest_eigsh
-
+from math import log
+from sklearn.metrics import precision_recall_curve
 import matplotlib.pyplot as plt
 import pylab
 from scipy.spatial.distance import *
 from pyemd import emd
 from os import listdir
+
+def compare_sig(sig1,sig2,k):
+	sum=0.0
+	for i in range(0,k):
+		diff=sig1[i]-sig2[i]
+		sum=sum+(diff*diff)
+	return sum
+
+
 #Reference Dataset
-def eigen_signature(vec):
-        sum_vec = []
-        vec_sorted = []
-        '''no of eigen vectors provided'''
-        len_vec = len(vec)
-	print "len_vec is" + str(len_vec)
-	#print vec
-        for i in range(0,len_vec):
-                sum_vec.append(0.0)
-
-        for i in range(0,len_vec):
-                '''Sorting the eigen vectors '''
-                vec_sorted.append(sorted(vec[i],reverse=True))
-                '''Calculating sum in each eigen vector '''
-                for j in range(0,len(vec[i])):
-                        sum_vec[i] = sum_vec[i] + vec[i][j]
-
-        ''' Initializing the k_min - Analogous to top k '''
-        k_min = sys.maxint
-        for i in range(0,len_vec):
-                k = 0
-                sum_k = 0.0
-                ratio = 0.0
-                ''' calculating k value for which energy is greater than 90% '''
-                while (ratio < 0.9):
-                        sum_k = sum_k + vec_sorted[i][k]
-                        ratio = sum_k/sum_vec[i]
-                        k = k + 1
-                        print "ratio of i: "+str(ratio)
-                print "k of i: "+str(k)
-                '''Finding min of all such k values '''
-                k_min = min(k_min, k)
-        print "k_min is: "+str(k_min)
-
-        '''Getting the signatures which contain top k eigen values '''
-        sig = []
-        for i in range(0,len_vec):
-                sig.append([])
-                for j in range(0, k_min):
-                        sig[i].append(vec_sorted[i][j])
-	return sig
-
 def main():
         '''Eigen vectors generated for 3 graphs '''
 #        vec = [[1,2,3,4,5,6,7,8,9],[4,3,6,5,15],[40,50,60,70,80,90]]
 #        print "given eigen vectors"
 #        print vec
 Graph_List = []
+eigenk=[]
 dataset = [3]
+evals_list=[]
 index = 0
+num_graphs=0
+Nodes=100
 #Reference Dataset
 dic_list = []
-ground_data = []
-test_data = []
 for i in dataset:
         folder_name = '/home/anusha/CompBio/AlignmentFreeNetwork/comsig-master/dataset_'+str(i) + '/'
-        print folder_name
         filenames = [f for f in listdir(folder_name)]
         for filename_out in filenames:
-            #print filename_out
+            print filename_out
+	    num_graphs=num_graphs+1;
+	    vec_name='vec'+str(i)
+	    evals_name='evals'+str(i)
             model_name_o = filename_out.split('_')[0]
             Graph_List.append(folder_name+filename_out)
 #           print Graph_List
             graph = nx.read_edgelist(folder_name+filename_out)
-L = nx.adjacency_matrix(graph)
-evals,vec = np.linalg.eigh(L)
-#ind = np.argsort(evals)
-#evals = evals[ind]
-#vec = vec[:,ind]
-print "evals are "
-#print vec
-signatures = eigen_signature(vec)
-print "signatures are: "
-print signatures
+#	    nx.draw(graph)
+#	    plt.show()
+	    A = nx.adjacency_matrix(graph)
+#	    print (A.todense())
+	    '''
+	    D = np.zeros((100, 100))
+	    for i in range(0,100):
+	    	deg = 0
+        	for j in range(0,100):
+            		deg = deg +  int(A[i,j])
+        		D[i,i] = deg
+	    L = np.zeros((100,100))
+
+	    for i in range(0,100):
+        	for j in range(0,100):
+            		L[i,j] = D[i,j] - A[i,j]
+	    '''
+	    evals_name,vec_name = np.linalg.eigh(A)
+#	    print evals_name
+	    vec=evals_name.tolist()
+	    evals_list.append(evals_name)
+            sum_vec = 0.0
+            vec_sorted = []
+	    '''no of eigen vectors provided'''
+            len_vec = len(vec)
+            print "len_vec is" + str(len_vec)
+#	    for i in range(0,len_vec):
+#		vec_sorted.append(sorted(vec[i],reverse=True))
+            for j in range(0,len(vec)):
+                sum_vec = sum_vec+evals_name[j]
+	    print "sum_vec"+ str(sum_vec) 
+	    ''' Initializing the k_min - Analogous to top k '''
+	    k_min = sys.maxint
+	    k = 0
+            sum_k = 0.0
+            ratio = 0.0
+            ''' calculating k value for which energy is greater than 90% '''
+#            for p in range(0,len(vec)):
+#		while (ratio < 0.9):
+			#print "type of "
+#              		print type(vec_sorted[i][k])
+#			sum_k = sum_k + evals_name[p]
+#			print "evals_name[p]"+str(evals_name[p])
+#			print "sum_k" +str(sum_k)
+#                	ratio = sum_k/sum_vec
+#			print "ratio:"+ str(ratio)
+#                        k = k + 1
+#			print " k for p "+str(p)+ ":"+str(k) 
+	    eigenk.append(k)
+
+k=1
+print 'graphs numebr '+str(num_graphs)
+distance_matrix = np.ones((num_graphs,num_graphs))
+distance_matrix1 = []
+for i in  range(0,num_graphs):
+	for j in  range(i+1,num_graphs):
+		ret=compare_sig(evals_list[i],evals_list[j],k)
+		print 'ret:'+str(ret)
+		print 'round:'+str(round(ret,8))
+		distance_matrix[i][j] = ret
+            	distance_matrix[j][i] = distance_matrix[i][j]
+#		if (ret<1):
+#			distance_matrix1.append(1)
+#		else:
+#			distance_matrix1.append(0)
+print distance_matrix1
+ref = np.(len(distance_matrix1))
+precision, recall, thresholds = precision_recall_curve(ref,distance_matrix1)
+plt.plot(recall,precision,label='precision recall curve')
+plt.show()
 if __name__ == "__main__":
         main()
 
